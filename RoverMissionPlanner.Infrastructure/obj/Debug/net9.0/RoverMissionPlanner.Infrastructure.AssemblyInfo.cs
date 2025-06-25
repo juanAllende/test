b9b14bb207d20cb0ca87ef656845be239967a51a
Juan Carlos Allende <juancarlosallende@Juans-MacBook-Air.local>
2025-06-25 08:56:42 -0400

    feat: solución inicial RoverMissionPlanner (.NET 8, arquitectura de capas, endpoints, validaciones, tests)

--- RoverMissionPlanner.Infrastructure/obj/Debug/net9.0/RoverMissionPlanner.Infrastructure.AssemblyInfo.cs
+++ RoverMissionPlanner.Infrastructure/obj/Debug/net9.0/RoverMissionPlanner.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoverMissionPlanner.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd621b356a4482362949bd53c4fe878e4f669f8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoverMissionPlanner.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoverMissionPlanner.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
